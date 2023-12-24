9a91909846e99c8a65ea3e0167ace31a4742a9b2
Developer-Tarpley <developertarpley@gmail.com>
2023-12-23 20:44:40 -0700

    created projects section

--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -1,12 +1,42 @@
-
+import sodokuImg from '../stylesheets/img/sodokuapp.png'
 
 const Projects = () => {
     return (
         <section className="projects landingSections">
             <h2>PROJECTS</h2>
-            <p className="landingPTag">
+            <hr/>
+            <section className="projectSection">
+                
+                <div className="projectsDiv">
+                    <div className='viewDiv'>
+                        <button className='viewBtn'>view</button>
+                        <button className='viewBtn' >src code</button>
+                    </div>
+                    <img alt="sodoku game " src={sodokuImg} />
+                </div>
+                <div className="projectsDiv">
+                    <div className='viewDiv'>
+                        <button className='viewBtn'>view</button>
+                        <button className='viewBtn' >src code</button>
+                    </div>
+                    <img alt="sodoku game " src={sodokuImg} />
+                </div>
+                <div className="projectsDiv">
+                    <div className='viewDiv'>
+                        <button className='viewBtn'>view</button>
+                        <button className='viewBtn' >src code</button>
+                    </div>
+                    <img alt="sodoku game " src={sodokuImg} />
+                </div>
+                <div className="projectsDiv">
+                    <div className='viewDiv'>
+                        <button className='viewBtn'>view</button>
+                        <button className='viewBtn' >src code</button>
+                    </div>
+                    <img alt="sodoku game " src={sodokuImg} />
+                </div>
 
-            </p>
+            </section>
         </section>
     )
 }
